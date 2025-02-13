c1a603e80fbbd82f7b58c2ab88b1ad9ae1f5343d
Joshua Lant <joshualant@gmail.com>
2025-02-13 12:08:24 +0100

    ebtables: pointers to unions in uapi struct
    On morello architecture, use of kernel pointers in the uapi
    structures is not permitted, due to different alignment requirements
    between kernel and userspace. These pointers should be the new type
    __nf_kptr_t, whose size changes depening on the pc/non-pcUABI.
    Modify these to use a union which will be accessed in the kernel using
    the original member pointer, but will actually be of size __nf_kptr_t,
    avoiding heavy casting needed when using to new type directly.
    
    Signed-off-by: Joshua Lant <joshualant@gmail.com>
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- include/uapi/linux/netfilter_bridge/ebtables.h
+++ include/uapi/linux/netfilter_bridge/ebtables.h
@@ -125,7 +125,10 @@ struct ebt_entry_match {
 			char name[EBT_EXTENSION_MAXNAMELEN];
 			__u8 revision;
 		};
-		struct xt_match *match;
+		union {
+			struct xt_match *match;
+			__nf_kptr_t __match;
+		};
 	} u;
 	/* size of data */
 	unsigned int match_size;
@@ -138,7 +141,10 @@ struct ebt_entry_watcher {
 			char name[EBT_EXTENSION_MAXNAMELEN];
 			__u8 revision;
 		};
-		struct xt_target *watcher;
+		union {
+			struct xt_target *watcher;
+			__nf_kptr_t __watcher;
+		};
 	} u;
 	/* size of data */
 	unsigned int watcher_size;
@@ -151,7 +157,10 @@ struct ebt_entry_target {
 			char name[EBT_EXTENSION_MAXNAMELEN];
 			__u8 revision;
 		};
-		struct xt_target *target;
+		union {
+			struct xt_target *target;
+			__nf_kptr_t __target;
+		};
 	} u;
 	/* size of data */
 	unsigned int target_size;
