08e60a6bffe50084786e7334b91d3005279ed94c
Kevin Brodsky <kevin.brodsky@arm.com>
2025-02-24 09:02:54 +0000

    nvme: Silence warning in !PCuABI
    Commit "io_uring: Add explicit capability checks" modified
    io_uring_cmd_import_fixed() to take a user pointer instead of an
    unsigned long. The NVMe driver will need to be adapted accordingly
    (as per the comment) to support PCuABI, but that change also
    introduced a warning in !PCuABI. Silence it with an appropriate
    cast.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- drivers/nvme/host/ioctl.c
+++ drivers/nvme/host/ioctl.c
@@ -182,7 +182,7 @@ static int nvme_map_user_request(struct request *req, u64 ubuffer,
 		if (WARN_ON_ONCE(flags & NVME_IOCTL_VEC))
 			return -EINVAL;
 		/* TODO [PCuABI]: change ubuffer type to void __user * */
-		ret = io_uring_cmd_import_fixed(ubuffer, bufflen,
+		ret = io_uring_cmd_import_fixed((void __user *)ubuffer, bufflen,
 				rq_data_dir(req), &iter, ioucmd);
 		if (ret < 0)
 			goto out;
